6bd255c59a3a8662f6058208e25084e62765622d
jaensen <daniel@circles.name>
2022-11-11 13:49:26 +0100

    use crc_capacity_graph_2 instead of crc_capacity_graph for the initial update

--- CirclesUBI.PathfinderUpdater.IndexerDb/Queries.cs
+++ CirclesUBI.PathfinderUpdater.IndexerDb/Queries.cs
@@ -7,9 +7,14 @@ public static class Queries
         return $"select block_number from transaction_2 where hash = '{txHash}';";
     }
 
+    // public const string CapacityGraph = @"
+    //             select token_holder, can_send_to, token_owner, capacity::text
+    //             from crc_capacity_graph
+    //             where capacity > 0;";
+
     public const string CapacityGraph = @"
-                select token_holder, can_send_to, token_owner, capacity::text
-                from crc_capacity_graph
+                select ""from"", ""to"", token_owner, capacity::text
+                from crc_capacity_graph_2
                 where capacity > 0;";
 
     public static string GetChanges(long sinceBlock)
