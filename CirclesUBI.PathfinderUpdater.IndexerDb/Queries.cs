178729c0fd9d5f80293933742ec22ce672eed0b0
jaensen <daniel@circles.name>
2023-02-22 17:47:52 +0100

    filter negative balances from the export

--- CirclesUBI.PathfinderUpdater.IndexerDb/Queries.cs
+++ CirclesUBI.PathfinderUpdater.IndexerDb/Queries.cs
@@ -5,7 +5,8 @@ public static class Queries
     public const string BalancesBySafeAndToken = @"
         select safe_address, token_owner, balance::text
         from cache_crc_balances_by_safe_and_token
-        where safe_address != '0x0000000000000000000000000000000000000000';
+        where safe_address != '0x0000000000000000000000000000000000000000'
+        and balance > 0;
     ";
     
     public const string BalancesBySafeAndTokenAt25476000 = @"
